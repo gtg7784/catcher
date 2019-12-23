e008294625e5046471b74f47ad7bb3d491b8618e
고태건 <gtg7784@naver.com>
2019-12-23 09:44:29 +0900

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -165,7 +165,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 6
   },
   searchTitle: {
     fontSize: 12,
